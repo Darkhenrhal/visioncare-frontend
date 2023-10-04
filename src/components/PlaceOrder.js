c18b16357ae4cfbd966678ec90737ef37091c4dc
nuduja <nudujabandara@gmail.com>
2023-10-04 23:18:03 +0530

    customer table added

--- src/components/PlaceOrder.js
+++ src/components/PlaceOrder.js
@@ -1,6 +1,6 @@
-// Home.js
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function PlaceOrder() {  
 
@@ -9,11 +9,6 @@ function PlaceOrder() {
   const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
-    // Fetch data from your API and set it in the state
-    // Replace 'fetchDataFromAPI' with your actual API call
-    // fetchDataFromAPI().then((data) => {
-    //   setCustomerData(data);
-    // });
     fetchDataFromAPI();
   }, []);
 
@@ -24,22 +19,94 @@ function PlaceOrder() {
         throw new Error('Network response was not ok');
       }
       const data = await response.json();
-      setCustomerData(data);
+      setCustomerData(data.customers);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
-  };
+  };  
 
   const handleRowClick = (customer) => {
     setSelectedCustomer(customer);
   };
 
-  const handleSearch = () => {
+  const handleSearch = async () => {
     // Filter data based on searchText and update the state
     const filteredData = customerData.filter((customer) =>
       customer.customerID.includes(searchText)
     );
+    if (searchText == ""){
+      try {
+        const response = await fetch('http://localhost:4000/api/customers');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setCustomerData(data.customers);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    } else{
     setCustomerData(filteredData);
+    }
+  };
+
+  // // Function to delete a customer by their ID
+  // const handleDeleteCustomer = async (customerID) => {
+  //   try {
+  //     const response = await fetch(`http://localhost:4000/api/customers/${customerID}`, {
+  //       method: 'DELETE',
+  //     });
+  //     console.log(customerID);
+
+  //     if (!response.ok) {
+  //       throw new Error('Network response was not ok');
+  //     }
+
+  //     // Remove the deleted customer from the state
+  //     setCustomerData((prevData) => prevData.filter((customer) => customer.customerID !== customerID));
+  //   } catch (error) {
+  //     console.error('Error deleting customer:', error);
+  //   }
+  // };
+
+  // Function to navigate to the edit-customer page with the selected customer's ID
+  // const handleEditCustomer = (customerID) => {
+  //   // history.push(`/edit-customer/${customerID}`);
+  // };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSelectedCustomer({ ...selectedCustomer, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Send a POST request to your API endpoint
+      const response = await axios.post('http://localhost:4000/api/customers/create', selectedCustomer);
+
+      // Check if the request was successful
+      if (response.status === 200) {
+        console.log('Order added successfully:', response.data);
+        // Reset the form after successful submission
+        setSelectedCustomer({
+          customerID: '',
+          customerName: '',
+          address: '',
+          pNumber: '',
+          birthDate: '',
+          gender: '',
+          nic: '',
+          orders: '',
+          channelling: '',
+          surgery: '',
+        });
+      } else {
+        console.error('Error adding order:', response.data);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
@@ -76,6 +143,7 @@ function PlaceOrder() {
               <th>Orders</th>
               <th>Channelling</th>
               <th>Surgery</th>
+              {/* <th>Action</th> */}
             </tr>
           </thead>
           <tbody>
@@ -91,6 +159,12 @@ function PlaceOrder() {
                 <td>{customer.orders}</td>
                 <td>{customer.channelling}</td>
                 <td>{customer.surgery}</td>
+                {/* <td>
+                  
+                  <button onClick={() => handleDeleteCustomer(customer._id)}>Delete</button>
+                 
+                  <button onClick={() => handleEditCustomer(customer.customerID)}>Edit</button>
+                </td> */}
               </tr>
             ))}
           </tbody>
@@ -102,7 +176,7 @@ function PlaceOrder() {
         {/* Render your form here using the selectedCustomer state */}
         {selectedCustomer && (
           <div>
-            <h2>Selected Customer</h2>
+            {/* <h2>Selected Customer</h2>
             <p>Customer ID: {selectedCustomer.customerID}</p>
             <p>Customer Name: {selectedCustomer.customerName}</p>
             <p>Address: {selectedCustomer.address}</p>
@@ -112,8 +186,114 @@ function PlaceOrder() {
             <p>NIC: {selectedCustomer.nic}</p>
             <p>Orders: {selectedCustomer.orders}</p>
             <p>Channelling: {selectedCustomer.channelling}</p>
-            <p>Surgery: {selectedCustomer.surgery}</p>
+            <p>Surgery: {selectedCustomer.surgery}</p> */}
+
+            <h2>Place Order</h2>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label>Customer ID:</label>
+                  <input
+                    type="text"
+                    name="customerID"
+                    value={selectedCustomer.customerID}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label>Customer Name:</label>
+                  <input
+                    type="text"
+                    name="customerName"
+                    value={selectedCustomer.customerName}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label>Address:</label>
+                  <input
+                    type="text"
+                    name="address"
+                    value={selectedCustomer.address}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label>Phone Number:</label>
+                  <input
+                    type="text"
+                    name="pNumber"
+                    value={selectedCustomer.pNumber}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label>Birth Date:</label>
+                  <input
+                    type="text"
+                    name="birthDate"
+                    value={selectedCustomer.birthDate}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label>Gender:</label>
+                  <input
+                    type="text"
+                    name="gender"
+                    value={selectedCustomer.gender}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label>NIC:</label>
+                  <input
+                    type="text"
+                    name="nic"
+                    value={selectedCustomer.nic}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label>Orders:</label>
+                  <input
+                    type="text"
+                    name="orders"
+                    value={selectedCustomer.orders}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label>Channelling:</label>
+                  <input
+                    type="text"
+                    name="channelling"
+                    value={selectedCustomer.channelling}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div>
+                  <label>Surgery:</label>
+                  <input
+                    type="text"
+                    name="surgery"
+                    value={selectedCustomer.surgery}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <button type="submit">Place Order</button>
+              </form>
           </div>
+          
         )}
       </div>
     </div>
